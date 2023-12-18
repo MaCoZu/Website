c1c6989affe336a5cfe59cfc752cbf7179a1a178
MaCoZu <marcoz@posteo.de>
2023-12-18 15:25:47 +0100

    postcss vite autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./docs/*.{html,js}",
+    "./*.{html,js}",
     "./node_modules/flowbite/**/*.js"
 ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
     require('flowbite/plugin'),
+    require('@tailwindcss/typography'),
   ],
 }
 
